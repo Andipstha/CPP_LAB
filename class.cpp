14fe6d1e18518526c5e63f97eaddc54b76b206f5
KIST Student3 <student3@sddb.student>
2022-02-18 15:02:45 +0545

    modified

--- class.cpp
+++ class.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
 using namespace std;
+
 class X{
+	private:
 	int id;
 	char studentName[25];
 	char studentAddress[25];
-	char studentFacilty
+	char studentFaculty[4];
+	public:
+	void name()
+	{
+	
+		studentName[25]=a[25];
+	}
+	void address()
+	{
+		studentAddress[25]=b[25];
+	}
+	void faculty()
+	{
+		studentFaculty[4]=c[4];
+	}
 	
 };
+int main(){
+	char a[25];
+	char b[25];
+	char c[4];
+	
+}
