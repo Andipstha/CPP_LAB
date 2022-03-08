97fc1aa8d97642c12c7b6f5e4f23fd8925edfa62
KIST Student3 <student3@sddb.student>
2022-03-08 15:00:05 +0545

    intia

---
+++ classTask/Random/try.cpp
@@ -0,0 +1,26 @@
+//Wap to take the input of the 10 employee
+#include<iostream>
+using namespace std;
+class x{
+	
+	public:
+			input(int x){
+				string name;
+				int salary;
+				cout<<"Enter the name of the employeee"<<endl;
+				getline(cin,name);
+				fflush;
+				cout<<"Enter the salary"<<endl;
+				cin>>salary;
+				return 0;
+			}
+};
+int main(){
+	int x=5;
+	int i=0;
+	for (i=0;i<=x;i++){
+		
+		returnn 0;
+	}
+return 0;
+}
