ebc49d098c6a4a703c5ae9ad6962391da76ad4a1
KIST Student3 <student3@sddb.student>
2022-03-16 15:01:30 +0545

    added code

--- classTask/Exam/friend_function.cpp
+++ classTask/Exam/friend_function.cpp
@@ -11,7 +11,13 @@ class X
 			cout<<"Enter the value of x : ";
 			cin>>x;	
 		}	
-		friend void find(X );
+		void find(Test t) 
+		{
+		   if (t.x > t1.y) {
+		       cout << "Largest is:" << t.x;
+		   } else {
+		       cout << "Largest is:" << t1.y;
+  		 }
 		
 };
 class Y
@@ -25,4 +31,35 @@ class Y
 			cin>>y;	
 		}
 };
+#include<iostream>
+using namespace std;
 
+class Test {
+private:
+   int x, y;
+public:
+
+   void input() {
+       cout << "Enter two numbers:";
+       cin >> x>>y;
+   }
+
+   friend void find(Test t);
+};
+
+void find(Test t) {
+   if (t.x > t.y) {
+       cout << "Largest is:" << t.x;
+   } else {
+       cout << "Largest is:" << t.y;
+   }
+}
+
+int main() {
+
+   Test t;
+   t.input();
+   find(t);
+
+   return 0;
+}
